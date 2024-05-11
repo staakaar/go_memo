c47314096ccd20f73534899112f477be887b7e6d
staakaar <vancyroletaka@gmail.com>
2024-05-12 00:00:16 +0900

    add: enum

---
+++ fruit.go
@@ -0,0 +1,21 @@
+package main
+
+type Fruit int
+
+const (
+	Apple Fruit = iota
+	Orange
+	Banana
+)
+
+// func (i Fruit) String() string {
+// 	switch i {
+// 	case Apple:
+// 		return "Apple"
+// 	case Orange:
+// 		return "Orange"
+// 	case Banana:
+// 		return "Banana"
+// 	}
+// 	return fmt.Sprintf("Fruit(%d)", i)
+// }
