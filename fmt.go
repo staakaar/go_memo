ba47acd8f5b23cdb13886f9e2446f4a567eae422
staakaar <vancyroletaka@gmail.com>
2024-05-12 00:00:41 +0900

    add fmt module

---
+++ fmt.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+type F struct {
+	Name string
+	Age  int
+}
+
+func (f *F) String() string {
+	return fmt.Sprintf("Name=%q, AGE=%d", f.Name, f.Age)
+}
+
+func init() {
+	f := &F{
+		Name: "Taro",
+		Age:  24,
+	}
+
+	fmt.Printf("%v\n", f)
+
+	/** フィールド名と内容 */
+	fmt.Printf("%#v+\n", f)
+
+	/** struct名も出力 */
+	fmt.Printf("%#v+\n", f)
+
+	/** 型名を出力 */
+	fmt.Printf("%T\n", f)
+	fmt.Printf("%T\n", *f)
+}
--- init.go
+++ init.go
@@ -1,3 +1,12 @@
 package main
 
-func init() {}
+/** 上から順番にinit関数が呼ばれる */
+/** SQLドライバーなどの初期化に使用するケースがある */
+// _ "init/init1"
+// _ "init/init2"
+
+var name = "Jiro"
+
+func init() {
+	println("hi" + name)
+}
