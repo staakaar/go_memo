ed948a04455a5ceaf80fdcf65681b0b08e6b85ca
staakaar <vancyroletaka@gmail.com>
2024-05-14 00:15:00 +0900

    chore: add net impl

--- net.go
+++ net.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+func handleData(conn net.Conn) {
+	defer conn.Close()
+
+	var b [512]byte
+	for {
+		n, err := conn.Read(b[:])
+		if err != nil {
+			break
+		}
+		fmt.Print(string(b[:n]))
+	}
+}
+
+func init() {
+	lis, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lis.Close()
+
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleData(conn)
+	}
+}
