582a99454c74cf7c61ebf4623ec0a5c97c838c7a
staakaar <vancyroletaka@gmail.com>
2024-05-18 17:29:15 +0900

    chore: embed.go

--- embed.go
+++ embed.go
@@ -3,8 +3,13 @@
 package main
 
 import (
+	"embed"
 	_ "embed"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path"
 
 	"github.com/labstack/echo/v4"
 )
@@ -18,4 +23,31 @@ func main() {
 		return c.Blob(http.StatusOK, "image/png", contents)
 	})
 	e.Logger.Fatal(e.Start(":8989"))
+}
+
+//go:embed static
+var local embed.FS
+
+func init() {
+	fis, err := local.ReadDir("static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, fi := range fis {
+		in, err := local.Open(path.Join("static", fi.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		out, err := os.Create("embed-" + path.Base(fi.Name()))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		io.Copy(out, in)
+		out.Close()
+		in.Close()
+		log.Println("expected", "embed-"+path.Base(fi.Name()))
+	}
 }
