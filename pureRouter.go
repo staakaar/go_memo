e7f6ce7ed71df5fd231eb3e04b10a3d5f909e811
staakaar <vancyroletaka@gmail.com>
2024-05-24 23:19:52 +0900

    add: fileserver

--- pureRouter.go
+++ pureRouter.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"path"
 	"regexp"
 )
 
@@ -54,5 +56,48 @@ func (rt *router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	rt := router{}
 
+	rt.GET(`^/$`, func(p routerParam, w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello")
+	})
+
+	rt.GET(`^/(?P<name>\w+)$`, func(p routerParam, w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello: %v\n", p["name"])
+	})
+
+	rt.POST(`^/api$`, func(p routerParam, w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "text/json")
+		fmt.Fprintln(w, `{"status": "OK"}`)
+	})
+
+	http.ListenAndServe(":8080", &rt)
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.Handle("/public/", http.FileServer(http.Dir("./static")))
+	// publicを取り除く
+	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("./static"))))
+
+	fileserver := http.StripPrefix("/public/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if path.Ext(r.URL.Path) == ".xls" {
+			w.Header().Set("Content-Type", "application/vnd.ms-excel")
+		}
+		fileserver.ServeHTTP(w, r)
+	})
+
+	mimemap := map[string]string{
+		".xls":  "application/vnd.ms-excel",
+		".xlsx": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+		".ppt":  "application/vnd.ms-powerpoint",
+		".pptx": "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+		".doc":  "application/msword",
+		".docx": "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if typ, ok := mimemap[path.Ext(r.URL.Path)]; ok {
+			w.Header().Set("Content-Type", typ)
+		}
+		fileserver.ServeHTTP(w, r)
+	})
 }
