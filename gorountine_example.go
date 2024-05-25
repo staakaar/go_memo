8ee2630acf20895462843743c1543a9d497c1fe4
staakaar <vancyroletaka@gmail.com>
2024-05-26 00:27:37 +0900

    chore: add goroutine

---
+++ gorountine_example.go
@@ -0,0 +1,24 @@
+//go:build ignore
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func doSomething(wg *sync.WaitGroup, id int) {
+	defer wg.Done()
+	for i := 0; i < 10000; i++ {
+		fmt.Printf("%d\n", id)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go doSomething(&wg, i)
+	}
+	wg.Wait()
+}
